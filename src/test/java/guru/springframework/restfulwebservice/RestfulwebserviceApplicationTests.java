cf96ad2148dc6c57dd3d876e571bc60e421df6ec
BrandonBoyWonder <brandon.bob.moore@gmail.com>
2020-09-10 09:14:54 +0100

    project init

---
+++ src/test/java/guru/springframework/restfulwebservice/RestfulwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
